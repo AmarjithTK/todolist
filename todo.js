d3a7ca59d0b817b4db50ea854e09fc962756b4a0
trinity <AmarjithTK>
2020-04-06 09:08:50 +0530

    Added delete cookie option

--- todo.js
+++ todo.js
@@ -1,37 +1,135 @@
+
+// all the initial variables goes here
+
 const addform=document.querySelector('.add')
 
 const list=document.querySelector('.todos')
 
 const search = document.querySelector('.search input')
 
+const li = document.querySelectorAll('li')
 
 
 
-addform.addEventListener('submit',e=>{
-    e.preventDefault();
 
-    const todo= addform.add.value.trim();
-    console.log(todo)
 
-    if(todo.length){
-     generateTemplate(todo);
-     console.log(todo.length)
-     addform.reset()
+
+// function to store the backup todos in cookies 
+
+
+
+const getIndex=()=>{
+    return Number(Cookies.get('cookiepos'))
+
+}
+
+let CookiePos = getIndex()
+
+
+let CookieIndex=0
+
+
+const AddItem = (string) =>{
+
+    if(CookiePos>CookieIndex){
+        CookieIndex=CookiePos
+    }
+
+    let CookieCount=`array${CookieIndex}`
+    console.log(CookieIndex)
+
+    Cookies.set(CookieCount,string,{ expires: 35 })
+
+    CookieIndex++
+
+    Cookies.set('cookiepos',CookieIndex,{ expires: 35 })
+
+
+
+
+
+}
+
+
+
+// function to retrive the stored items from the cookies
+
+const RetriveItem = ()=>{
+
+  
+    
+    if(CookiePos>=0){
+        for (let index = 0; index < CookiePos; index++) {
+            let CookieQuery= `array${index}`
+            
+           let CookieString=Cookies.get(CookieQuery)
+           let type = typeof CookieString
+           console.log(type)
+
+           if(type=='string')
+            generateTemplate(CookieString)
+           
+            
+        }
     }
 
 
+}
+
+
+
+// Event listener to  add a new todo to the list 
+
+addform.addEventListener('submit',e=>{
+  
+    e.preventDefault()
+
+    const todo = addform.add.value.trim()
+   
+
+    if(todo.length){
+     generateTemplate(todo)
+     addform.reset()
+     AddItem(todo)
+   
+    }
+    RemoveSearch()
+
+
 })
 
 
+// Function to remove items in the todolist based upon the user request through delete button
+
 list.addEventListener('click',e =>{
+
+    
+
+
+    let target = e.target;
+    let li = target.closest('li'); // get reference
+    let nodes = Array.from( li.closest('ul').children ); // get array
+    let index = nodes.indexOf( li ); 
+
+    RemoveCookie(index)
+    
+
+    console.log(index)
+   
    if(e.target.classList.contains('delete'))
 {
     e.target.parentElement.remove();
+    RemoveSearch()
 }
+
 })
 
 
 
+
+
+
+// function to generate template string for the list
 
 const generateTemplate= todo =>{
 
@@ -43,7 +141,12 @@ const html = `<li class="list-group-item d-flex justify-content-between align-it
 list.innerHTML+=html;
 
 
+
 }
+
+
+
+// function that allows us to search for a particular item in the list
 
 search.addEventListener('keyup',(a)=>{
 
@@ -52,8 +155,13 @@ a.preventDefault()
 Findstring(search.value)
 
 
+RemoveAdd(search.value)
+
+
 })
 
+
+// function to actually search for the item triggered by the former function
 
 
 const Findstring= string =>{
@@ -74,14 +182,67 @@ const Findstring= string =>{
         
 }
 
-var y=[5,6,7]
 
-Cookies.set('amar', y)
-
-var x = Cookies.get('amar')
+// removing the new todo form when search operation is undergoing
 
 
-console.log(x)
+
+const RemoveAdd = (searchlen) => {
+addform.style.display='none'
+if(searchlen.length==0)
+{
+    addform.style.display='block'
+}
+}
+
+
+// function to remove the search bar when no element is added
+
+
+const RemoveSearch= ()=>{
+
+    let content = Array.from(document.querySelectorAll('li')).length
+    if(content===0){
+    search.style.display='none'}
+    else
+    search.style.display='block'
+
+}
+
+
+
+
+
+
+
+window.onload=function onload(){
+  
+    RetriveItem()
+    RemoveSearch()
+}
+
+
+
+
+
+const RemoveCookie=(index)=>{
+    const keyword=`array${index}`
+    Cookies.remove(keyword)
+    
+}
+
+
+
+
+// Cookies.set('amar', 'dsg')
+// Cookies.set('amar', 'sdfgdsg')
+
+
+// var x = Cookies.get('amar')
+
+// console.log(x)
+
+
 
 
 
